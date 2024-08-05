9c541b2176dc2a5a3c26737e104c4fd757836549
Joseph Crump <josephcrump79@yahoo.com>
2024-08-05 10:44:50 -0500

    arith lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
-// Write your code here
+// multiplication (section 1)
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2; 
+// random (section 2)
+const random = Math.floor(Math.random()*10 + 1);
+const num3 = 25;
+const num4 = 7;
+const mod = num3 % num4;
+const max= Math.max (20);
